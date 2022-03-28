5ef1532ae3a4517e8ba143d4720b2ac5f233b701
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2022-03-27 23:38:11 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from BrainFuck import *
+
+Debbuger(True)
+run_interpreter("BrainFuck/test.bfk") # <- File for read BrainFuck code. You can change for another file
+
+# Compatible files: .bf, .bfk, .b, .fuck, .brainf
